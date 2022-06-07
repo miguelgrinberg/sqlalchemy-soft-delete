95f5cd61769c225b1d2f59ef221c2721aa3bf61f
Juan Márquez <juanvmarquezl@users.noreply.github.com>
2022-06-07 08:33:39 -0400

    Update app.py
    Prevent field from being null (field required)

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128))
-    deleted = db.Column(db.Boolean(), default=False)
+    deleted = db.Column(db.Boolean(), default=False, nullable=False)
 
     query_class = QueryWithSoftDelete
 
